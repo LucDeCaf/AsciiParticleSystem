3c26c80617e6f132e900529804c146b144ac4bf7
LucDeCaf <ldecafmeyer@gmail.com>
2024-04-29 11:24:59 +0200

    add Vector2 helper functions

--- src/vector2.rs
+++ src/vector2.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 pub struct Vector2<T> {
     pub x: T,
     pub y: T,
@@ -6,5 +7,28 @@ pub struct Vector2<T> {
 impl<T> Vector2<T> {
     pub fn new(x: T, y: T) -> Self {
         Self { x, y }
+    }
+}
+
+impl Vector2<f32> {
+    pub fn floor(&self) -> Self {
+        let mut floored = self.clone();
+        floored.x = self.x.floor();
+        floored.y = self.y.floor();
+        floored
+    }
+
+    pub fn ceil(&self) -> Self {
+        let mut floored = self.clone();
+        floored.x = self.x.ceil();
+        floored.y = self.y.ceil();
+        floored
+    }
+
+    pub fn round(&self) -> Self {
+        let mut floored = self.clone();
+        floored.x = self.x.round();
+        floored.y = self.y.round();
+        floored
     }
 }
